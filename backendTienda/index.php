34cc014d04719faa6a96c4df2a0a3bb4a54b4a3f
DavidApRey <57902001+DavidApRey@users.noreply.github.com>
2024-02-26 05:54:46 -0500

    act

---
+++ backendTienda/index.php
@@ -0,0 +1,20 @@
+<?php
+
+include('./conexion.php');
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+if ($metodo == 'POST') {
+    
+}
+
+if ($metodo == 'GET') {
+    
+}
+
+if ($metodo == 'PUT') {
+    
+}
+
+if ($metodo == 'DELETE') {
+    
+}
